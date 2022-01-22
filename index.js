e887b36126fcf58bf144ddaf250020f7c3a362d9
regularPark <90092440+JayPark0110@users.noreply.github.com>
2022-01-22 11:10:45 +0900

    first

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "park", age = 29, gender = "male";
+const sayHI = (name, age, gender) => {
+    console.log(`Hello My name is ${name}, I'm ${age} and ${gender}`);
+};
+sayHI(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("typescript");
+const name = "park", age = 29, gender = "male";
+
+const sayHI = (name, age, gender) => {
+    console.log(`Hello My name is ${name}, I'm ${age} and ${gender}`);
+}
+
+sayHI(name, age, gender);
+
+export {};
